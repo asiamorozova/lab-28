d584395b731887ffa3697203b6e23907180bf064
Anastasia Morozova <morozova.asia@gmail.com>
2020-04-30 16:57:14 -0700

    added service

--- services/x-files.js
+++ services/x-files.js
@@ -3,4 +3,7 @@ export const fetchCharacters = () => {
     .then(res => res.json());
 };
 
-export const fetchById = () =>
+export const fetchById = () => {
+  return fetch(`https://dragon-age-api.herokuapp.com/api/v1/characters/${_id}`)
+    .then(res => res.json());
+};
