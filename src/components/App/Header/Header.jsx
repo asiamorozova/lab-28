ca2acddeec77946ad9b17b15df4a7adc9ff29ec2
Anastasia Morozova <morozova.asia@gmail.com>
2020-04-29 17:17:43 -0700

    header component

---
+++ src/components/App/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <Link to="/">
+      <h1>X-Files: I want to believe</h1>
+    </Link>
+             
+
+  </header>
+);
+
+export default Header;
+
